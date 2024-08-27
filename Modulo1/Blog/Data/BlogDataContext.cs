e23fc7fa1543fb053cc3c889ad134f2d00e123a7
Douglas Borges <douglasborges19@gmail.com>
2024-08-26 23:33:14 -0300

    Create

--- Modulo1/Blog/Data/BlogDataContext.cs
+++ Modulo1/Blog/Data/BlogDataContext.cs
@@ -6,12 +6,12 @@ namespace Blog.Data
     public class BlogDataContext : DbContext
     {
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Category> Posts { get; set; }
-        public DbSet<Category> PostTags { get; set; }
-        public DbSet<Category> Roles { get; set; }
-        public DbSet<Category> Tags { get; set; }
-        public DbSet<Category> Users { get; set; }
-        public DbSet<Category> UserRoles { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        // public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        // public DbSet<UserRole> UserRoles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True");
--- Modulo1/Blog/Program.cs
+++ Modulo1/Blog/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Blog.Data;
+using Blog.Models;
 
 namespace Blog
 {
@@ -10,7 +11,16 @@ namespace Blog
         {
             using (var context = new BlogDataContext())
             {
-                
+                var tag = new Tag
+                {
+                    Name = "ASP.NET",
+                    Slug = "aspnet"
+                };
+
+                context.Tags.Add(tag);
+
+                // Salva no banco de dados
+                context.SaveChanges();
             }
         }
     }
