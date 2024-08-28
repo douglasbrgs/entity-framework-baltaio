5148883f02549d22ed835b00bfa859bd0ce1684e
Douglas Borges <douglasborges19@gmail.com>
2024-08-28 19:55:03 -0300

    Table, Key e Identity

---
+++ Modulo2/Blog/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BlogDataContext())
+            {
+
+            }
+        }
+    }
+}
