c1709db5c9248d7440b9659bb503d1ab38dbe69a
Douglas Borges <douglasborges19@gmail.com>
2024-08-28 22:19:54 -0300

    Chave primaria e identity

--- Modulo3/Blog/Data/Mappings/CategoryMap.cs
+++ Modulo3/Blog/Data/Mappings/CategoryMap.cs
@@ -9,6 +9,12 @@ namespace Blog.Data.Mappings
         public void Configure(EntityTypeBuilder<Category> builder)
         {
             builder.ToTable("Category");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn(); // PRIMARY KEY IDENTITY (1, 1)
         }
     }
 }
