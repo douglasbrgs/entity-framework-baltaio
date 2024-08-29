8ac45355a0a2b2bfa8a94d0143df6264af7dc458
Douglas Borges <douglasborges19@gmail.com>
2024-08-28 21:52:01 -0300

    Include

--- Modulo2/Blog/Program.cs
+++ Modulo2/Blog/Program.cs
@@ -12,36 +12,48 @@ namespace Blog
         {
             using var context = new BlogDataContext();
 
-            var user = new User
-            {
-                Name = "Douglas Borges",
-                Slug = "douglasborges",
-                Email = "douglas@borges.com",
-                Bio = "Mestre Eng Computação",
-                Image = "https://borges.com",
-                PasswordHash = "12334543543"
-            };
+            // var user = new User
+            // {
+            //     Name = "Douglas Borges",
+            //     Slug = "douglasborges",
+            //     Email = "douglas@borges.com",
+            //     Bio = "Mestre Eng Computação",
+            //     Image = "https://borges.com",
+            //     PasswordHash = "12334543543"
+            // };
 
-            var category = new Category
-            {
-                Name = "Backend",
-                Slug = "Backend"
-            };
+            // var category = new Category
+            // {
+            //     Name = "Backend",
+            //     Slug = "Backend"
+            // };
 
-            var post = new Post
-            {
-                Author = user,
-                Category = category,
-                Body = "<p>Hello world</p>",
-                Slug = "comecando-com-ef-core",
-                Summary = "Neste artigo vamos aprender EF Core",
-                Title = "Começando com EF Core",
-                CreateDate = DateTime.Now,
-                LastUpdateDate = DateTime.Now,
-            };
+            // var post = new Post
+            // {
+            //     Author = user,
+            //     Category = category,
+            //     Body = "<p>Hello world</p>",
+            //     Slug = "comecando-com-ef-core",
+            //     Summary = "Neste artigo vamos aprender EF Core",
+            //     Title = "Começando com EF Core",
+            //     CreateDate = DateTime.Now,
+            //     LastUpdateDate = DateTime.Now,
+            // };
 
-            context.Posts.Add(post);
-            context.SaveChanges();
+            // context.Posts.Add(post);
+            // context.SaveChanges();
+
+            var posts = context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Author)
+                .OrderByDescending(x => x.LastUpdateDate)
+                .ToList();
+
+            foreach (var post in posts)
+            {
+                Console.WriteLine($"{post.Title} escrito por {post.Author?.Name}");
+            }
         }
     }
 }
