62e48eafa5e84715e7eab1096ee27560033b59de
Douglas Borges <douglasborges19@gmail.com>
2024-09-19 08:49:31 -0300

    Testando os relacionamentos

--- Modulo3/Blog/Program.cs
+++ Modulo3/Blog/Program.cs
@@ -11,6 +11,36 @@ namespace Blog
         static void Main(string[] args)
         {
             using var context = new BlogDataContext();
+
+            context.Users.Add(new User{
+                Bio = "Mestre Engenheiro de Computação",
+                Email = "douglasborges19@gmail.com",
+                Image = "https://borges.br",
+                Name = "Douglas Borges",
+                PasswordHash = "1234",
+                Slug = "douglas-borges"
+            });
+
+            context.SaveChanges();
+
+            var user = context.Users.FirstOrDefault();
+            var category = new Category
+            {
+                Name = "Backend",
+                Slug = "backend"
+            };
+            var post = new Post
+            {
+                Author = user,
+                Body = "Meu artigo",
+                Category = category,
+                CreateDate = System.DateTime.Now,
+                Slug = "meu-artigo",
+                Summary = "Nesse artigo vamos conferir...",
+                Title = "Meu artigo"
+            };
+            context.Posts.Add(post);
+            context.SaveChanges();
         }
     }
 }
